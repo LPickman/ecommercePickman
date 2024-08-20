d299cf92ca297d7f926c850520e19b839b5af9b2
LPickman <pickmanleandro1@gmail.com>
2024-08-20 14:56:05 -0300

    Se agregaron imagenes

--- src/asynmock.js
+++ src/asynmock.js
@@ -5,7 +5,8 @@ const items = [
     descripcion: "Camiseta de algodón de alta calidad",
     precio: 19.99,
     categoria: "ropa",
-    imagen: "https://via.placeholder.com/200?text=Imagen+1",
+    imagen:
+      "https://http2.mlstatic.com/D_NQ_NP_846116-MLA31035477403_062019-O.webp",
     stock: 50,
   },
   {
@@ -14,7 +15,8 @@ const items = [
     descripcion: "Pantalón vaquero estilo casual",
     precio: 29.99,
     categoria: "ropa",
-    imagen: "https://via.placeholder.com/200?text=Imagen+2",
+    imagen:
+      "https://celadasa.vtexassets.com/arquivos/ids/245263-800-auto?v=638357461507670000&width=800&height=auto&aspect=true",
     stock: 30,
   },
   {
@@ -23,7 +25,8 @@ const items = [
     descripcion: "Remera de algodón blanca",
     precio: 9.99,
     categoria: "ropa",
-    imagen: "https://via.placeholder.com/200?text=Imagen+3",
+    imagen:
+      "https://dcdn.mitiendanube.com/stores/001/205/102/products/remera-lisa-fr-rj-11-ef4b6ca4b08c3f434315906905869409-640-0.jpg",
     stock: 50,
   },
   {
@@ -32,7 +35,8 @@ const items = [
     descripcion: "Zapatos de cuero negros",
     precio: 49.99,
     categoria: "calzado",
-    imagen: "https://via.placeholder.com/200?text=Imagen+4",
+    imagen:
+      "https://equipovallejo.vtexassets.com/arquivos/ids/257832-800-auto?v=638301436097000000&width=800&height=auto&aspect=true",
     stock: 20,
   },
   {
@@ -41,7 +45,8 @@ const items = [
     descripcion: "Bolso de mano elegante",
     precio: 39.99,
     categoria: "accesorios",
-    imagen: "https://via.placeholder.com/200?text=Imagen+5",
+    imagen:
+      "https://d368r8jqz0fwvm.cloudfront.net/7401-product_lg/bolso-para-viaje-trango-ii-new-60-lts.jpg",
     stock: 15,
   },
   {
@@ -50,7 +55,8 @@ const items = [
     descripcion: "Reloj de pulsera deportivo",
     precio: 59.99,
     categoria: "accesorios",
-    imagen: "https://via.placeholder.com/200?text=Imagen+6",
+    imagen:
+      "https://www.bidcom.com.ar/publicacionesML/productos/RELMO009/1000x1000-RELMO009.jpg",
     stock: 10,
   },
   {
@@ -59,7 +65,7 @@ const items = [
     descripcion: "Gafas de sol polarizadas",
     precio: 24.99,
     categoria: "accesorios",
-    imagen: "https://via.placeholder.com/200?text=Imagen+7",
+    imagen: "https://m.media-amazon.com/images/I/51UFqgNG3jL._AC_SL1500_.jpg",
     stock: 25,
   },
 ];
